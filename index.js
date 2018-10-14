032cdc68cedcca3ca83042a62bbc5cd669f8e7f2
Ellen Dunn <edunn227@gmail.com>
2018-10-14 16:15:19 -0500

    javascript

---
+++ index.js
@@ -0,0 +1,13 @@
+
+$(document).ready(function() {
+  $('#bio-button').click(function() {
+    document.getElementById('bio').scrollIntoView({behavior: "smooth", block: "start"})
+  })
+})
+
+$(document).ready(function() {
+  $('#projects-button').click(function() {
+    document.getElementById('projects').scrollIntoView({behavior: "smooth", block: "start"})
+  })
+})
+
